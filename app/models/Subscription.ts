f0c7a0fcc5276279f969ac5d3404cbff2c9ebc63
Fabian Boni <58946541+FabianBoni@users.noreply.github.com>
2024-10-25 12:01:15 +0200

    Update Subscription.ts

--- app/models/Subscription.ts
+++ app/models/Subscription.ts
@@ -6,6 +6,7 @@ const SubscriptionSchema = new mongoose.Schema({
   stripePriceId: String,
   stripeSubscriptionId: String,
   status: String,
+  searchCount: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
