1b3cda6cffaf8bdc319e7f8ba54d4be618ac6d99
Fabian Boni <58946541+FabianBoni@users.noreply.github.com>
2024-10-25 19:31:56 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve('stream-browserify')
+      };
+      return config;
+    }
+  }
+  
+  module.exports = nextConfig  
