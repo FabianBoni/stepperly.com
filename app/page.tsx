a0c4e212ce5bf6f3de2788f08857fd5d709a54bc
Fabian Boni <58946541+FabianBoni@users.noreply.github.com>
2024-10-25 19:41:00 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from 'react';
-import { useUser } from '@clerk/nextjs';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
